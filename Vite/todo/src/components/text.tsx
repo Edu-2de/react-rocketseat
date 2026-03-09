8f2e03b56d9aa4d067a0bda87f7d17be3524d78c
Eduardo Da Silva Paim <166542880+Edu-2de@users.noreply.github.com>
2026-03-09 12:56:01 -0300

    feat: refactor Text component and extract textVariants for improved styling management

--- Vite/todo/src/components/text.tsx
+++ Vite/todo/src/components/text.tsx
@@ -1,42 +1,22 @@
-import React from "react"
-import { cva, type VariantProps } from "class-variance-authority";
-
-const textVariants = cva('font-sans text-gray-400', {
-    variants:{
-        variant: {
-            'body-sm-bold': 'text-sm leading-5 font-semibold',
-            'body-md': 'text-base leading-6 font-normal',
-            'body-md-bold' : 'text-base leading-6 font-semibold',
-        }
-    },
-    defaultVariants: {
-        variant: 'body-md'
-    }
-})
+import React from "react";
+import { type VariantProps } from "class-variance-authority";
+import { textVariants } from "./text.variants";
 
 interface TextProps extends VariantProps<typeof textVariants> {
-    as? : keyof React.JSX.IntrinsicElements;
-    className?: string,
+    as?: keyof React.JSX.IntrinsicElements;
+    className?: string;
     children?: React.ReactNode;
-
 }
 
-const Text = ({
-    as = 'span',
-    variant,
-    className,
-    children,
-    ...props
-} : TextProps) => {
+const Text = ({ as = 'span', variant, className = '', children, ...props }: TextProps) => {
     return React.createElement(
         as,
         {
-            className: className ? textVariants({ variant, className }) : textVariants({ variant }),
+            className: textVariants({ variant, className }),
             ...props
         },
         children
-    )
-}
+    );
+};
 
-
-export default Text;
+export default Text; // O Rei reina sozinho aqui!
