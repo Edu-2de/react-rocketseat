55ec5ea661d0616ac24864b12088e1adf152d3d3
Eduardo Da Silva Paim <166542880+Edu-2de@users.noreply.github.com>
2026-03-10 13:50:03 -0300

    feat: add ButtonIcon component with variants and props support

---
+++ Vite/todo/src/components/button-icon.tsx
@@ -0,0 +1,56 @@
+import React from "react";
+import { Icon } from "./icon";
+import { cva, type VariantProps } from "class-variance-authority";
+
+export const buttonIconVariants = cva(`
+    inline-flex items-center justify-center cursor-pointer
+    transition group
+`, {
+    variants: {
+        variant: {
+            primary: 'bg-green-base hover:bg-green-dark',
+            secondary: 'bg-pink-light hover:bg-pink-dark',
+            tertiary: 'bg-transparent hover:bg-gray-200'
+        },
+        size: {
+            sm: 'w-6 h-6 p-1 rounded'
+        },
+        disabled: {
+            true: 'opacity-50 pointer-events-none'
+        }
+    },
+    defaultVariants: {
+        variant: 'primary',
+        size: 'sm',
+        disabled: false
+    }
+})
+
+export const buttonIconIconVariants = cva('transition', {
+    variants: {
+        variant: {
+            primary: 'fill-white'
+        }
+    }
+}, )
+
+interface ButtonIconProps extends
+    VariantProps<typeof buttonIconVariants>,
+        Omit<React.ComponentProps<'button'>, 'size' | 'disabled'> {
+    icon: React.ComponentProps<typeof Icon>['svg']
+}
+
+export const ButtonIcon = ({
+    variants,
+    size,
+    disabled,
+    className,
+    icon: IconComponent,
+    ...props
+}) => {
+    return(
+        <button>
+            <Icon svg={}></Icon>
+        </button>
+    )
+}
