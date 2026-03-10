23e1f025a2a3337d2c3547743dd9ad555eb388a1
Eduardo Da Silva Paim <166542880+Edu-2de@users.noreply.github.com>
2026-03-10 10:09:24 -0300

    feat: add SVGR support and refactor icon handling
    - Updated package.json to include vite-plugin-svgr for SVG imports.
    - Refactored App.tsx to use the new Icon component for SVG icons.
    - Created a new Icon component to handle SVG rendering with animation support.
    - Updated vite.config.ts to include the SVGR plugin.
    - Added vite-env.d.ts for type definitions related to Vite and SVGR.

--- Vite/todo/src/App.tsx
+++ Vite/todo/src/App.tsx
@@ -1,5 +1,13 @@
-import Text from "./components/text"
-import trashIcon from './assets/icons/Trash.svg'
+import Text from "./components/text";
+import { Icon } from "./components/icon"
+
+import TrashIcon from './assets/icons/Trash.svg?react';
+import CheckIcon from './assets/icons/Check.svg?react';
+import ClipBoardIcon from './assets/icons/ClipboardText.svg?react';
+import PencilIcon from './assets/icons/Pencil.svg?react';
+import PlusIcon from './assets/icons/Plus.svg?react';
+import SpinnerIcon from './assets/icons/Spinner.svg?react';
+import XIcon from './assets/icons/X.svg?react';
 
 export default function App() {
 
@@ -27,7 +35,13 @@ export default function App() {
         </Text>
 
         <div className="flex gap-1">
-          <img src={trashIcon} alt="trash" className="fill-pink-base"/>
+          <Icon svg={TrashIcon} className="fill-pink-base" />
+          <Icon svg={CheckIcon} className="fill-green-base" />
+          <Icon svg={PlusIcon} className="fill-green-base" />
+          <Icon svg={SpinnerIcon} animate/>
+          <Icon svg={PencilIcon} className="fill-green-base" />
+          <Icon svg={XIcon} className="fill-green-base" />
+          <Icon svg={ClipBoardIcon} className="fill-pink-dark" />
         </div>
 
       </div>
