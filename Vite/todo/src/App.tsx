b4de68cc4c9a6fcfcddb163288773c90552bfe27
Eduardo Da Silva Paim <166542880+Edu-2de@users.noreply.github.com>
2026-03-11 13:55:38 -0300

    feat: add Skeleton component to App for loading placeholders

--- Vite/todo/src/App.tsx
+++ Vite/todo/src/App.tsx
@@ -7,6 +7,7 @@ import { InputText } from "./components/input-text";
 import { InputCheckbox } from "./components/input-checkbox";
 import { Card } from "./components/card";
 import { Container } from "./components/container";
+import { Skeleton } from "./components/skeleton";
 
 import TrashIcon from './assets/icons/Trash.svg?react';
 import CheckIcon from './assets/icons/Check.svg?react';
@@ -15,6 +16,7 @@ import PencilIcon from './assets/icons/Pencil.svg?react';
 import PlusIcon from './assets/icons/Plus.svg?react';
 import SpinnerIcon from './assets/icons/Spinner.svg?react';
 import XIcon from './assets/icons/X.svg?react';
+
 
 
 
@@ -70,6 +72,12 @@ export default function App() {
 
           <div>
             <Card size='md'>Olá mundo!</Card>
+          </div>
+
+          <div className="space-y-2">
+            <Skeleton className="h-6"/>
+            <Skeleton className="h-6"/>
+            <Skeleton className="w-96 h-6"/>
           </div>
 
         </div>
