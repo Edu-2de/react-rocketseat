9193136b0bd3ddf0f5d68e5328db5ee16fadcdf5
Eduardo Da Silva Paim <166542880+Edu-2de@users.noreply.github.com>
2026-03-11 18:06:10 -0300

    refactor: improve code formatting and organization in App and Badge components

--- Vite/todo/src/components/badge.tsx
+++ Vite/todo/src/components/badge.tsx
@@ -1,86 +1,84 @@
-import React from "react"
-import Text from "./text"
-import { cva, cx, type VariantProps } from "class-variance-authority"
+import { cva, cx, type VariantProps } from "class-variance-authority";
+import type React from "react";
+
 import { Skeleton } from "./skeleton";
+import Text from "./text";
 
 export const badgeVariants = cva(
-    'inline-flex items-center justify-center rounded-full',
-    {
-        variants: {
-            variant: {
-                none: '',
-                primary: 'bg-green-light',
-                secondary: 'bg-pink-light'
-            },
-            size: {
-                sm: 'py-0.5 px-2'
-            }
-        },
-        defaultVariants: {
-            variant: 'primary',
-            size: 'sm'
-        }
-    }
+	"inline-flex items-center justify-center rounded-full ",
+	{
+		variants: {
+			variant: {
+				none: "",
+				primary: "bg-green-light",
+				secondary: "bg-pink-light",
+			},
+			size: {
+				sm: "py-0.5 px-2",
+			},
+		},
+		defaultVariants: {
+			variant: "primary",
+			size: "sm",
+		},
+	},
 );
 
-export const badgeTextVariants = cva('', {
-    variants: {
-        variant: {
-            none: '',
-            primary: 'text-green-dark',
-            secondary: 'text-pink-dark'
-        }
-    },
-    defaultVariants: {
-        variant: 'primary'
-    }
-})
+export const badgeTextVariants = cva("", {
+	variants: {
+		variant: {
+			none: "",
+			primary: "text-green-dark",
+			secondary: "text-pink-dark",
+		},
+	},
+	defaultVariants: {
+		variant: "primary",
+	},
+});
 
-export const badgeSkeletonVariants = cva('', {
-    variants: {
-        size: {
-            sm: 'w-6 h-6'
-        }
-    },
-    defaultVariants: {
-        size: 'sm'
-    }
-})
+export const badgeSkeletonVariants = cva("", {
+	variants: {
+		size: {
+			sm: "w-6 h-6",
+		},
+	},
+	defaultVariants: {
+		size: "sm",
+	},
+});
 
-interface BadgeProps extends React.ComponentProps<'div'>,
-    VariantProps<typeof badgeVariants> {
-        loading?: boolean
-    }
+interface BadgeProps
+	extends React.ComponentProps<"div">,
+		VariantProps<typeof badgeVariants> {
+	loading?: boolean;
+}
 
 export const Badge = ({
-    variant,
-    size,
-    className,
-    children,
-    loading,
-    ...props
+	variant,
+	size,
+	className,
+	children,
+	loading,
+	...props
 }: BadgeProps) => {
+	if (loading) {
+		return (
+			<Skeleton
+				rounded={"full"}
+				className={cx(
+					badgeVariants({ variant: "none" }),
+					badgeSkeletonVariants({ size }),
+				)}
+			/>
+		);
+	}
 
-    if(loading){
-        return(
-            <Skeleton
-                rounded={'full'}
-                className={cx(
-                    badgeVariants({variant: 'none'}),
-                    badgeSkeletonVariants({size})
-                )}
-            />
-        )
-    }
-
-    return(
-        <div className={badgeVariants({variant, size, className})} {...props}>
-            <Text
-                variant='body-sm-bold'
-                className={badgeTextVariants({variant})}
-            >
-                {children}
-            </Text>
-        </div>
-    )
-}
+	return (
+		<div className={badgeVariants({ variant, size, className })} {...props}>
+			<Text variant="body-sm-bold" className={badgeTextVariants({ variant })}>
+				{children}
+			</Text>
+		</div>
+	);
+};
