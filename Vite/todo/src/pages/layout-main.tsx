c7816f6f021daa6107dd8d3cce412d4219da9da6
Eduardo Da Silva Paim <166542880+Edu-2de@users.noreply.github.com>
2026-03-12 12:12:02 -0300

    feat: add Footer component and refactor LayoutMain to use it

--- Vite/todo/src/pages/layout-main.tsx
+++ Vite/todo/src/pages/layout-main.tsx
@@ -1,5 +1,5 @@
-import { NavLink, Outlet } from "react-router";
-import Text from "../components/text";
+import { Outlet } from "react-router";
+import { Footer } from "../core-components/footer";
 import { Header } from "../core-components/header";
 import { MainContent } from "../core-components/main-content";
 
@@ -10,20 +10,7 @@ export const LayoutMain = () => {
 			<MainContent>
 				<Outlet />
 			</MainContent>
-			<footer className="my-5 md:my-10">
-				<nav className="flex items-center justify-center gap-4">
-					<NavLink to="/">
-						<Text variant="body-sm-bold" className="text-gray-300">
-							Tarefas
-						</Text>
-					</NavLink>
-					<NavLink to="/components">
-						<Text variant="body-sm-bold" className="text-gray-300">
-							Componentes
-						</Text>
-					</NavLink>
-				</nav>
-			</footer>
+			<Footer />
 		</>
 	);
 };
